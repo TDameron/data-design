6d26dbe19636965e5b4c353a5892a1a9fb0114cb
Thomas Dameron <tdameron1@cnm.edu>
2020-01-14 16:26:00 -0700

    Created a more complete user story and added user pre and post conditions.

--- epic/user-story.php
+++ epic/user-story.php
@@ -8,7 +8,6 @@
 	<h1>User Story</h1>
 	<ul>
 		<li>As a freelancer, Johnathan should be able to quickly and efficiently negotiate jobs with current and prospective clients</li>
-		<li>He can achieve this by creating a Fiverr profile and posting his portfolio, allowing clients to find him</li>
 	</ul>
 	<a href="index.php">Back to Index</a>
 </body>
