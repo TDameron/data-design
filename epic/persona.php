e533b8e131eacc506deaa037caadff33e2868945
Thomas Dameron <tdameron1@cnm.edu>
2020-01-14 16:40:28 -0700

    Fleshed out the User Persona.

--- epic/persona.php
+++ epic/persona.php
@@ -17,7 +17,13 @@
 <div>
 	<ul>
 	<li>User is a professional graphic designer who plans to use Fiverr to make money as a side job</li>
-	<li>Fiverr is tailor made for the user to find jobs quickly and easily over other options such as Craigslist</li>
+	<li>User is a big fan of entertainment, often uses streaming programs such as Spotify and Bandcamp to listen to music while working on graphic design</li>
+	<li>Enjoys popular movies such as the Marvel and Star Wars films</li>
+	<li>User is politically neutral that prefers to stay away from incendiary political topics</li>
+	<li>Is well-versed in, and enjoys, using technology such as the latest phone model and products that facilitate digital art such as Photoshop and Wacom products</li>
+	<li>As a graphic designer, user would appreciate a website that is very clean and easy to look at that is also fully featured without being overly complex</li>
+	<li>The user is working for clients on Fiverr as a side job, he may have a variety of obligations at any given time</li>
+	<li>Is possibly in student debt, the in-studio job market may not be the best right now</li>
 	</ul>
 </div>
 <a href="index.php">Back to Index</a>
