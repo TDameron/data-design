da94abf68fa6901d7110d3b53bde07f997fd215b
Thomas Dameron <tdameron1@cnm.edu>
2020-01-14 10:11:30 -0700

    Persona draft 1 created.

---
+++ epic/persona.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="eng">
+<head>
+	<meta charset="utf-8">
+	<title>Persona</title>
+</head>
+<body>
+	<h1>Johnathan</h1>
+	<img src="Data-design-image.jpg" alt="The image didn't load :("> 
+	<ul>
+		<li>Age: 30</li>
+		<li>Gender: Male</li>
+		<li>Occupation: Graphic designer</li>
+		<li>Technology: Macpro with Cintiq graphics tablet</li>
+	</ul>
+</body>
