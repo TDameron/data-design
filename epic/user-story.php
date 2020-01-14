f60f6c51608083ee1bf7b7b36e7470015aee9160
Thomas Dameron <tdameron1@cnm.edu>
2020-01-14 13:03:50 -0700

    Elaborated on User Story

--- epic/user-story.php
+++ epic/user-story.php
@@ -9,9 +9,11 @@
 		<li>Johnathan is a graphic designer who is partially self-taught and has his bachelors degree in graphic design</li>
 		<li>Johnathan has created a profile on Fiverr</li>
 		<li>Johnathan has been contacted by the owner of a small business who wants a logo designed for their food truck</li>
+		<li>Johnathan opens the message and replies by sending a resume and starts a chat through Fiverr's chat system</li>
 		<li>The client reads Johnathan's credentials and resume and negotiates the terms and conditions through fiverr</li>
-		<li>Johnathan sends several drafts of the logo to be reviewed by the client</li>
-		<li>The logo is sent to the client by the deadline, the client pays Johnathan as per the conditions outlined in their agreement</li>
+		<li>Johnathan sends several drafts of the logo to be reviewed by the client via email as well as update via Fiverr messages</li>
+		<li>The final logo is sent to the client for a final review/li>
+		<li>the client pays Johnathan via paypal and is given access to the photoshop files for the final product for personal/business use</li>
 	</ul>
 	<a href="index.php">Back to Index</a>
 </body>
