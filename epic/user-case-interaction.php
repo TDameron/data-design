638811729b35f90414fb8925f16dcfa8232a0318
Thomas Dameron <tdameron1@cnm.edu>
2020-01-15 13:18:36 -0700

    Edited user case interaction to incoporate browser responses.

--- epic/user-case-interaction.php
+++ epic/user-case-interaction.php
@@ -5,22 +5,27 @@
 	<title>User Case Interaction</title>
 <body>
 <h1>User Case Interaction</h1>
-<p><strong>User Precondition: </strong> Johnathan is looking to make extra money doing smaller jobs that require his graphic design skillset</p>
+<p><strong>User Precondition: </strong> Johnathan is looking to make extra money doing smaller jobs that require his graphic design skillset.</p>
 <div>
 <ul>
-	<li>Johnathan is a graphic designer who is partially self-taught and has his bachelors degree in graphic design</li>
-	<li>Johnathan has created a profile on Fiverr and posted his resume under the "Art and Graphic Design" section</li>
-	<li>Johnathan has been contacted by the owner of a small business who wants a logo designed for their food truck</li>
-	<li>Johnathan opens the message and replies by sending a resume and starts a chat through Fiverr's chat system</li>
-	<li>The system logs the interaction between Johnathan and the client</li>
-	<li>The client reads Johnathan's credentials and resume and negotiates the terms and conditions through Fiverr</li>
-	<li>Fiverr server sends a Johnathan's contract to the client via Fiverr's email service</li>
-	<li>Johnathan sends several drafts of the logo to be reviewed by the client via email as well as update via Fiverr messages</li>
-	<li>The final logo is sent to the client for a final review</li>
-	<li>The client pays Johnathan via paypal and is given access to the photoshop files for the final product for personal/business use</li>
-	<li>Fiverr server automatically sends a receipt to Johnathan and the client and invites the client to leave a review for Johnathan</li>
+	<li>Johnathan is a graphic designer who is partially self-taught and has his bachelors degree in graphic design.</li>
+	<li>Johnathan has created a profile on Fiverr and posted his resume under the "Art and Graphic Design" section.</li>
+	<li><strong>Johnathan's profile and information is now stored in the server and is able to log in through the browser.</strong></li>
+	<li>Johnathan has been contacted by the owner of a small business who wants a logo designed for their food truck.</li>
+	<li><strong>The browser gives Johnathan a notification that he has a new message.</strong></li>
+	<li>Johnathan opens the message and replies by sending a resume and starts a chat through Fiverr's chat system.</li>
+	<li><strong>The browser gives Johnathan a notification that his message and resume were sent.</strong></li>
+	<li>The client reads Johnathan's credentials and resume and negotiates the terms and conditions through Fiverr.</li>
+	<li><strong>Fiverr server sends Johnathan's contract to the client via Fiverr's messaging service.</strong></li>
+	<li>Johnathan sends several drafts of the logo to be reviewed by the client via email as well as update via Fiverr messages.</li>
+	<li><strong>The browser lets Johnathan know that his messages and drafts were successfully sent to to client.</strong></li>
+	<li>The final logo is sent to the client for a final review.</li>
+	<li><strong>The browser lets Johnathan know that he has sent his final draft and will alert him when the client responds.</strong></li>
+	<li>The client pays Johnathan via paypal and is given access to the photoshop files for the final product for personal/business use.</li>
+	<li><strong>Johnathan recieves a notification through Fiverr that his final draft was accepted</strong></li>
+	<li><strong>Fiverr server automatically sends a receipt to Johnathan and the client and invites the client to leave a review for Johnathan.</strong></li>
 </ul>
 </div>
-<p><strong>User Postcondition: </strong> Johnathan was able to acquire and complete a small graphic design job in his spare time and has received payment from the client</p>
+<p><strong>User Postcondition: </strong> Johnathan was able to acquire and complete a small graphic design job in his spare time and has received payment from the client.</p>
 <a href="index.php">Back to Index</a>
 </body>
